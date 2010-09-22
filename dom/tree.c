627c70996b31be3cdbfbac56027c17d6a4885303
Frederico Gonçalves <frederico.lopes.goncalves@gmail.com>
2010-09-23 00:24:36 +0100

    First things on red black tree

--- dom/tree.c
+++ dom/tree.c
@@ -1,3 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "tree.h"
+#include "list.h"
 #include "node.h"
 
+/*WARNNING left_rotate assumes that rotate_on->right is NOT NULL and that root->parent IS NULL*/
+static void left_rotate(tree_node* root, tree_node* rotate_on){
+  tree_node* y = rotate_on->right;
+  rotate_on->right = y->left;
+
+  if(y->left != NULL)
+    y->left->parent = rotate_on;
+  
+  y->parent = rotate_on->parent;
+ 
+  if(rotate_on->parent == NULL)
+    root = y;
+  else
+    if(rotate_on == rotate_on->parent->left)
+      rotate_on->parent->left = y;
+    else
+      rotate_on->parent->right = y;
+ 
+  y->left = rotate_on;
+  rotate_on->parent = y;
+  return;
+}
+
+void dfs_print(const tree_node* root, int pad, char* pos){
+  int i = 0;
+  if(root == NULL)
+    return;
+
+  for(; i < pad; i++) printf(" ");
+  printf("| %s node %s ", pos, key(root));
+  if(root->parent != NULL)
+    printf("with parent %s\n", key(root->parent));
+  else
+    printf("with no parent\n");
+  for(i = 0; i < pad; i++) printf(" ");
+  printf("\t+------\n");
+
+  dfs_print(root->left, pad + 1, "Left");
+  dfs_print(root->right, pad + 1, "Right");
+}
+
+int main(){
+  dom_node one, two, three, four;
+  tree_node tone, ttwo, tthree, tfour;
+
+  one.name = "1";
+  two.name = "2";
+  three.name = "3";
+  four.name = "4";
+  
+  tone.node = &one;
+  ttwo.node = &two;
+  tthree.node = &three;
+  tfour.node = &four;
+
+  tone.left = NULL;
+  tone.right = &ttwo;
+  tone.parent = NULL;
+
+  ttwo.left = NULL;
+  ttwo.right = &tthree;
+  ttwo.parent = &tone;
+
+  tthree.left = NULL;
+  tthree.right = NULL;
+  tthree.parent = &ttwo;
+
+  /*  tfour.left = NULL;
+  tfour.right = NULL;
+  tfour.parent = &tone;*/
+
+  dfs_print(&tone, 0, "Root");
+  left_rotate(&tone, &ttwo);
+  printf("\nRotated to:\n");
+  dfs_print(&tone, 0, "Root");
+  return 0;
+}
--- dom/tree.h
+++ dom/tree.h
@@ -1,11 +1,23 @@
 #ifndef __TREE_H__
 #define __TREE_H__
 
+#define key(NODE)\
+  (NODE->node->name)
+
+#define RED 1
+#define BLACK 2
+
 struct snode;
 
 typedef struct stree_node{
-  struct snode* left;
-  struct snode* right;
+  struct snode* node;
+
+  int color;
+  
+  struct stree_node* parent;
+  struct stree_node* left;
+  struct stree_node* right;
 }tree_node;
+
 
 #endif

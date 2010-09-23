072ea678b7969fd3668a40c74a956c9ae32b7e69
Frederico Gonçalves <frederico.lopes.goncalves@gmail.com>
2010-09-23 02:10:21 +0100

    cenas

--- dom/tree.c
+++ dom/tree.c
@@ -3,19 +3,37 @@
 #include "tree.h"
 #include "list.h"
 #include "node.h"
+#include "macros.h"
 
-/*WARNNING left_rotate assumes that rotate_on->right is NOT NULL and that root->parent IS NULL*/
-static void left_rotate(tree_node* root, tree_node* rotate_on){
+static tree_node* new_tree_node(dom_node* node){
+  tree_node *z = alloc(tree_node, 1);
+  
+  z->node = node;
+  z->parent = &NIL;
+  z->left = &NIL;
+  z->right = &NIL;
+  z->color = RED;
+  return z;
+}
+
+static tree_root* new_tree(){
+  tree_root* r = alloc(tree_root, 1);
+  r->root = &NIL;
+  return r;
+}
+
+/*WARNNING left_rotate assumes that rotate_on->right is NOT &NIL and that root->parent IS &NIL*/
+static void left_rotate(tree_root* root, tree_node* rotate_on){
   tree_node* y = rotate_on->right;
   rotate_on->right = y->left;
 
-  if(y->left != NULL)
+  if(y->left != &NIL)
     y->left->parent = rotate_on;
   
   y->parent = rotate_on->parent;
  
-  if(rotate_on->parent == NULL)
-    root = y;
+  if(rotate_on->parent == &NIL)
+    root->root = y;
   else
     if(rotate_on == rotate_on->parent->left)
       rotate_on->parent->left = y;
@@ -27,18 +45,18 @@ static void left_rotate(tree_node* root, tree_node* rotate_on){
   return;
 }
 
-/*WARNNING right_rotate assumes that rotate_on->left is NOT NULL and that root->parent IS NULL*/
-static void right_rotate(tree_node* root, tree_node* rotate_on){
+/*WARNNING right_rotate assumes that rotate_on->left is NOT &NIL and that root->parent IS &NIL*/
+static void right_rotate(tree_root* root, tree_node* rotate_on){
   tree_node* y = rotate_on->left;
   rotate_on->left = y->right;
 
-  if(y->right != NULL)
+  if(y->right != &NIL)
     y->right->parent = rotate_on;
   
   y->parent = rotate_on->parent;
  
-  if(rotate_on->parent == NULL)
-    root = y;
+  if(rotate_on->parent == &NIL)
+    root->root = y;
   else
     if(rotate_on == rotate_on->parent->right)
       rotate_on->parent->right = y;
@@ -50,14 +68,87 @@ static void right_rotate(tree_node* root, tree_node* rotate_on){
   return;
 }
 
+static void red_black_tree_insert_fixup(tree_root* root, tree_node* z){
+  tree_node* y;
+
+  while(z->parent->color == RED){
+    
+    if(z->parent == z->parent->parent->left){
+      y = z->parent->parent->right;
+      
+      if(y->color == RED){
+	z->parent->color = BLACK;
+	y->color = BLACK;
+	z->parent->parent->color = RED;
+	z = z->parent->parent;
+      }else{
+	if(z == z->parent->right){
+	  z = z->parent;
+	  left_rotate(root, z);
+	}
+	z->parent->color = BLACK;
+	z->parent->parent->color = RED;
+	right_rotate(root, z->parent->parent);
+      }
+      
+    }else{
+      y = z->parent->parent->left;
+      
+      if(y->color == RED){
+	z->parent->color = BLACK;
+	y->color = BLACK;
+	z->parent->parent->color = RED;
+	z = z->parent->parent;
+      }else{
+	if(z == z->parent->left){
+	  z = z->parent;
+	  right_rotate(root, z);
+	}
+	z->parent->color = BLACK;
+	z->parent->parent->color = RED;
+	left_rotate(root, z->parent->parent);
+      }
+    }
+    
+    root->root->color = BLACK;
+  }
+}
+
+void red_black_tree_insert(tree_root* root, dom_node* node){
+  tree_node *y = &NIL, *x = root->root;
+
+  tree_node *z = new_tree_node(node);
+
+  while(x != &NIL){
+    y = x;
+    if(compare(z, x) < 0)
+      x = x->left;
+    else
+      x = x->right;
+  }
+  
+  z->parent = y;
+
+  if(y == &NIL)
+    root->root = z;
+  else{
+    if(compare(z,y) < 0)
+      y->left = z;
+    else
+      y->right = z;
+  }
+
+  red_black_tree_insert_fixup(root, z);
+}
+
 void dfs_print(const tree_node* root, int pad, char* pos){
   int i = 0;
-  if(root == NULL)
+  if(root == &NIL)
     return;
 
   for(; i < pad; i++) printf(" ");
   printf("| %s node %s ", pos, key(root));
-  if(root->parent != NULL)
+  if(root->parent != &NIL)
     printf("with parent %s\n", key(root->parent));
   else
     printf("with no parent\n");
@@ -70,38 +161,16 @@ void dfs_print(const tree_node* root, int pad, char* pos){
 
 int main(){
   dom_node one, two, three, four;
-  tree_node tone, ttwo, tthree, tfour;
+  tree_root* r = new_tree();
 
   one.name = "1";
   two.name = "2";
   three.name = "3";
   four.name = "4";
+
+  red_black_tree_insert(r,&one);
+  red_black_tree_insert(r,&two);
   
-  tone.node = &one;
-  ttwo.node = &two;
-  tthree.node = &three;
-  tfour.node = &four;
-
-  tone.left = NULL;
-  tone.right = &ttwo;
-  tone.parent = NULL;
-
-  ttwo.left = NULL;
-  ttwo.right = &tthree;
-  ttwo.parent = &tone;
-
-  tthree.left = NULL;
-  tthree.right = NULL;
-  tthree.parent = &ttwo;
-
-  /*  tfour.left = NULL;
-  tfour.right = NULL;
-  tfour.parent = &tone;*/
-
-  dfs_print(&tone, 0, "Root");
-  left_rotate(&tone, &tone);
-  printf("\nRotated left to:\n");
-  dfs_print(&ttwo, 0, "Root");
-
+  dfs_print(r->root, 0, "Root");
   return 0;
 }
--- dom/tree.h
+++ dom/tree.h
@@ -1,8 +1,13 @@
 #ifndef __TREE_H__
 #define __TREE_H__
 
+#include <string.h>
+
 #define key(NODE)\
   (NODE->node->name)
+
+#define compare(NODE_A, NODE_B)\
+  (strcmp(key(NODE_A), key(NODE_B)))
 
 #define RED 1
 #define BLACK 2
@@ -19,5 +24,17 @@ typedef struct stree_node{
   struct stree_node* right;
 }tree_node;
 
+typedef struct sroot{
+  struct stree_node* root;
+}tree_root;
 
+tree_node NIL = {
+  .node = NULL,
+  .color = BLACK,
+  .parent = NULL,
+  .left = NULL,
+  .right = NULL
+};
+
+extern void red_black_tree_insert(tree_root* root, struct snode* node);
 #endif
